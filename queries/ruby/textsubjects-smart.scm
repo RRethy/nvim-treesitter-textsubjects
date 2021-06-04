7336bf19be885d1fc4938cab40f9ef96d77daa93
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-06-04 16:09:00 -0400

    Add Ruby support

---
+++ queries/ruby/textsubjects-smart.scm
@@ -0,0 +1,28 @@
+((comment) @_start @_end
+     (#make-range! "range" @_start @_end))
+
+; TODO This query doesn't work for comment groups at the start and end of a
+; file
+; See https://github.com/tree-sitter/tree-sitter/issues/1138
+(((_) @head . (comment) @_start . (comment)+ @_end (_) @tail)
+    (#not-has-type? @tail "comment")
+    (#not-has-type? @head "comment")
+    (#make-range! "range" @_start @_end))
+
+(([
+    (method)
+    (call)
+    (module)
+    (class)
+    (block)
+    (do_block)
+    (if)
+    (unless)
+    (for)
+    (until)
+    (while)
+] @_start @_end)
+(#make-range! "range" @_start @_end))
+
+((return (_) @_start @_end)
+    (#make-range! "range" @_start @_end))
