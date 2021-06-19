714c6c69ff0b13017fc5e60351a2533dbf20e95c
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-06-19 18:34:23 -0400

    add sortbet function annotation

--- queries/ruby/textsubjects-smart.scm
+++ queries/ruby/textsubjects-smart.scm
@@ -24,5 +24,10 @@
 ] @_start @_end)
 (#make-range! "range" @_start @_end))
 
+; sorbet type *annotation*
+(((call method: (identifier) @_start) . (method) @_end)
+    (#match? @_start "sig")
+    (#make-range! "range" @_start @_end))
+
 ((return (_) @_start @_end)
     (#make-range! "range" @_start @_end))
