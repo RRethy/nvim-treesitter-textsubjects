9780e793a7703959ca95a275ec9183101a3d6133
Stephen Prater <me@stephenprater.com>
2021-10-04 17:29:57 -0700

    Add Elixir

---
+++ queries/elixir/textsubjects-container-outer.scm
@@ -0,0 +1,6 @@
+(([
+    (call (module))
+    (call function: (function_identifier) (call) (do_block))
+    (binary_op left: (identifier) operator: "|>")
+] @_start @_end)
+(#make-range! "range" @_start @_end))
