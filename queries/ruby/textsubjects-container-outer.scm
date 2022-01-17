b6b5f3ebe953279284b5c08be986441c60f628d8
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2022-01-17 10:35:46 -0500

    Add singleton_method with sorbet type signature

--- queries/ruby/textsubjects-container-outer.scm
+++ queries/ruby/textsubjects-container-outer.scm
@@ -7,6 +7,6 @@
 (#make-range! "range" @_start @_end))
 
 ; sorbet type *annotation*
-(((call method: (identifier) @_start) . (method) @_end)
+(((call method: (identifier) @_start) . [(singleton_method) (method)] @_end)
     (#match? @_start "sig")
     (#make-range! "range" @_start @_end))
