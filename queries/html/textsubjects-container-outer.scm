20d2bd5ffd99a07f554eb75b56d1686a2de9775c
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2022-04-04 20:26:26 -0400

    Add html and yaml support

---
+++ queries/html/textsubjects-container-outer.scm
@@ -0,0 +1,1 @@
+(((element) @_start @_end) (#make-range! "range" @_start @_end))
---
+++ queries/yaml/textsubjects-container-outer.scm
@@ -0,0 +1,6 @@
+(([
+    (block_mapping_pair)
+    (block_node)
+    (document)
+] @_start @_end)
+(#make-range! "range" @_start @_end))
