0318a8b42c325e19c801b8f00737f776c9aaacb2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-08-02 14:02:20 +0200

    Remove jsx_fragment nodes from javascript and tsx queries
    jsx_fragment nodes have been removed from the javascript grammar [0]
    (which is used by the tsx grammar).
    
    JSX fragments are now regular JSX opening and closing nodes [1].
    
    Using these jsx_fragment nodes in the queries caused treesitter to crash
    when parsing the queries. It prevented using this plugin with JavaScript
    and TSX files.
    
    [0]: https://github.com/tree-sitter/tree-sitter-javascript/commit/bb1f97b643b77fc1f082d621bf533b4b14cf0c30#diff-919ac210accac9ecc55a76d10a7590e3d85ca3f0e165b52d30f08faee486d0cbL592
    [1]: https://github.com/tree-sitter/tree-sitter-typescript/commit/b893426b82492e59388a326b824a346d829487e8#diff-7641e7dab915695b385ec7ba3ca3bf8c7bad9639df6795de4eb4703a98fecc16

--- queries/javascript/textsubjects-smart.scm
+++ queries/javascript/textsubjects-smart.scm
@@ -21,7 +21,6 @@
     (switch_statement)
     ; jsx
     (jsx_element)
-    (jsx_fragment)
     (jsx_self_closing_element)
     (jsx_attribute)
 ] @_start @_end)
--- queries/tsx/textsubjects-big.scm
+++ queries/tsx/textsubjects-big.scm
@@ -5,7 +5,5 @@
     ; typescript
     (type_alias_declaration)
     (interface_declaration)
-    ; jsx
-    (jsx_fragment)
 ] @_start @_end)
 (#make-range! "range" @_start @_end))
