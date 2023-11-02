011c09784f1a95cf469a48c1993b664190440391
Timo Leistner <38843394+timoleistner@users.noreply.github.com>
2023-11-02 23:49:19 +0100

    Update treesitter node names (#41)
    * Update treesitter node names
    
    Update julias treesitter node names according to 
    https://github.com/RRethy/nvim-treesitter-textsubjects/issues/38
    
    Added additional node names too.
    
    * fixes E5108 invalid node type at position 2 for language julia

--- queries/julia/textsubjects-smart.scm
+++ queries/julia/textsubjects-smart.scm
@@ -1,23 +1,29 @@
-((comment) @_start @_end
+((line_comment) @_start @_end
      (#make-range! "range" @_start @_end))
 
 ; TODO This query doesn't work for comment groups at the start and end of a
 ; file
 ; See https://github.com/tree-sitter/tree-sitter/issues/1138
-(((_) @head . (comment) @_start . (comment)+ @_end (_) @tail)
-    (#not-has-type? @tail "comment")
-    (#not-has-type? @head "comment")
+(((_) @head . (line_comment) @_start . (line_comment)+ @_end (_) @tail)
+    (#not-has-type? @tail "line_comment")
+    (#not-has-type? @head "line_comment")
     (#make-range! "range" @_start @_end))
 
 (([
     (function_definition)
     (struct_definition)
+    (module_definition)
+    (macro_definition)
+    (abstract_definition)
     (while_statement)
     (for_statement)
     (if_statement)
     (try_statement)
     (do_clause)
-    (array_expression)
+    (matrix_expression)
+    (tuple_expression)
+    (vector_expression)
+    (compound_statement)
 ] @_start @_end)
 (#make-range! "range" @_start @_end))
 
@@ -30,10 +36,13 @@
 ((tuple_expression (_) @_start @_end . ","? @_end)
     (#make-range! "range" @_start @_end))
 
-((array_expression (_) @_start @_end . ","? @_end)
+((matrix_expression (_) @_start @_end . ","? @_end)
     (#make-range! "range" @_start @_end))
 
-((type_argument_list (_) @_start @_end . ","? @_end)
+((tuple_expression (_) @_start @_end . ","? @_end)
+    (#make-range! "range" @_start @_end))
+
+((vector_expression (_) @_start @_end . ","? @_end)
     (#make-range! "range" @_start @_end))
 
 ((return_statement (_) @_start @_end)
