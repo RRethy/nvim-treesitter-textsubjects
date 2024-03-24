9f9a6b307fb122f13708f78483222abd43b7bb3a
rktjmp <rktjmp@users.noreply.github.com>
2024-03-25 09:23:26 +1100

    Update fennel queries for new parser included in nvim-treesitter (#45)
    The new fennel parser included with nvim-treesitter uses different
    capture groups. Most behaviour is broadly the same but the new parser
    seems to select more white space than the previous parser.

--- queries/fennel/textsubjects-container-inner.scm
+++ queries/fennel/textsubjects-container-inner.scm
@@ -1,38 +1,26 @@
-; ([(symbol) (number) (string) (binding)] @_start @_end
-;   (#make-range! "range" @_start @_end))
+;; Symbol
+((symbol) @_start @_end
+  (#make-range! "range" @_start @_end))
+
+;; Strings
+;; :abc
+;;  ^^^
+((string_content) @_start @_end
+ (#make-range! "range" @_start @_end))
 
 ;; Inner Seq
 ;; [...]
 ;;  ^^^
-((sequential_table . (_) @_start @_end (_)? @_end .)
+((sequence . (_) @_start @_end (_)? @_end .)
  (#make-range! "range" @_start @_end))
 
 ;; Inner Assoc
 ;; {...}
 ;;  ^^^
-((table . (_) @_start @_end (_)? @_end .)
- (#make-range! "range" @_start @_end))
+ ((table . (_) @_start @_end (_)? @_end .)
+  (#make-range! "range" @_start @_end))
 
-;; Inner List
-;; (...)
-;;  ^^^
-((_ . "(" . _ @_start @_end _* @_end . ")" .)
- (#make-range! "range" @_start @_end))
-
-;; (local {...} ...)
-;;         ^^^
-((table_binding . "{" . _ @_start @_end _? @_end . "}" .)
- (#make-range! "range" @_start @_end))
-
-;; (let [a 10 b 20] (...))
-;;       ^^^^^^^^^
-((let_clause . "[" . _ @_start @_end _+ @_end . "]" .)
- (#make-range! "range" @_start @_end))
-
-;; iterators
-;; It's probably not practical to assume how these should expand, so we will
-;; just expand out to the binds
-;; (icollect [a 10 b 20] (...))
-;;            ^^^^^^^^^
-((iter_bindings) @_start @_end
+;; (x ...)
+;;  ^^^^^
+((list . (_) @_start (_)* @_end)
  (#make-range! "range" @_start @_end))
--- queries/fennel/textsubjects-container-outer.scm
+++ queries/fennel/textsubjects-container-outer.scm
@@ -1,37 +1,19 @@
-;; Outer Seq
-;; [...]
-;; ^^^^^
-((sequential_table) @_start @_end
+;; :abc "abc"
+;; ^^^^ ^^^^^
+((string) @_start @_end
  (#make-range! "range" @_start @_end))
 
-;; Outer Assoc
+;; [...]
+;; ^^^^^
+((sequence) @_start @_end
+ (#make-range! "range" @_start @_end))
+
 ;; {...}
 ;; ^^^^^
 ((table) @_start @_end
  (#make-range! "range" @_start @_end))
 
-;; Outer List
-;; (...)
-;; ^^^^^
-((_ . "(" @_start . _+ . ")" @_end .)
- (#make-range! "range" @_start @_end))
-
-;; (local {...} ...)
-;;        ^^^^^
-((table_binding . "{" @_start "}" @_end .)
- (#make-range! "range" @_start @_end))
-
-;; (let [a 10 b 20] (...))
-;;      ^^^^^^^^^^^
-((let_clause . "[" @_start _? "]" @_end .)
- (#make-range! "range" @_start @_end))
-
-;; (icollect [a 10 b 20] (...))
-;;           ^^^^^^^^^^^
-;; cant use [(icollect) ...] here
-((icollect "[" @_start (iter_bindings) "]" @_end)
- (#make-range! "range" @_start @_end))
-((collect "[" @_start (iter_bindings) "]" @_end)
- (#make-range! "range" @_start @_end))
-((each "[" @_start (iter_bindings) "]" @_end)
+;; (x ...)
+;; ^^^^^^^
+((list) @_start @_end
  (#make-range! "range" @_start @_end))
